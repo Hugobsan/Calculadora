2018d5e9c92b632eb9bb15be00b7d8c09723ea71
Azsures <87209706+Azsures@users.noreply.github.com>
2021-12-22 21:12:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-a = 0
-function Calc(a){
-  a = a/3;
-  a = 70 - a;
-  a = a*3/2;
-  a = parseFloat(a.toFixed(2));
-  return a;
+function calc_falta(ch, jaf){
+  ch = parseInt(ch)
+  if (ch<12){
+    ch = ch*18
+    ch = (ch *0.25).toFixed(0)
+  }
+  else
+    ch = (ch *0.25).toFixed(0)
+
+  if(jaf != 0)
+    ch = ch - jaf
+  return ch
+}
+
+function Calc_Nota(nota){
+  nota = nota/3;
+  nota = 70 - nota;
+  nota = parseFloat(nota*3/2).toFixed(1);
+  return nota;
 }
